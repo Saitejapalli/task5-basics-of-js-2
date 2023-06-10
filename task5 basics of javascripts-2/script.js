5c67a2657110dd2379373458a97c2c3c9396eb81
Saitejapalli <133526081+Saitejapalli@users.noreply.github.com>
2023-06-10 16:01:16 +0530

    Add files via upload

---
+++ task5 basics of javascripts-2/script.js
@@ -0,0 +1,90 @@
+//bodmas
+let number=4+5*10-(52+4);
+console.log(number);
+
+//datatypes//
+//integer
+let demo= 45;
+console.log(typeof(demo));
+
+//string
+let demo1= "45";
+console.log(typeof(demo1));
+
+//undefined
+let x;
+console.log(typeof(x));
+
+//null
+let y= null;
+console.log(typeof(y));
+
+//if else statement
+let number1=5;
+if(number1==4){      //block of code to be executed if condition is true
+    console.log("hello world");
+}
+else{
+    console.log("hello guys");
+}
+
+//else if statement
+let number3=5;
+let number4=6;
+let number5=7;
+
+if(number3==6){       
+    console.log("number5");
+}
+else if (number4=6){
+    console.log("number6");
+}
+
+//time
+let time=23;
+if (time<= 12) {
+    console.log("good morning");
+}
+else if(time <=15){
+    console.log("good afternoon");
+}
+else if(time<=18){
+    console.log("good evening");
+}
+else{
+    console.log("good night");
+}
+
+       //for loops
+let num= 0   //number 0-10
+for(num=0;num< 11; num ++)
+{
+    console.log(num);
+}
+        //number 10-0
+for(num= 10; num>= 0; num--)
+{
+    console.log(num);
+}
+
+//while loop
+
+let number7=0;
+while(number7<=10){
+    console.log(number7);
+    number7++;
+}
+
+
+
+
+
+
+
+
+
+
+
+
+
+
